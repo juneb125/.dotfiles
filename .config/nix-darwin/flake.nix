34b8e131ba3ca060225da9c4299f7d14efe90378
June B <jrbergeron823@gmail.com>
2024-10-18 14:06:32 -0500

    Install NeoVim through flake.nix

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -13,7 +13,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ pkgs.neovim
         ];
 
       # Auto upgrade nix package and the daemon service.
