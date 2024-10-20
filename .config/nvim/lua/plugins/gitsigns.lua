0d9e489cab7a7d99459de8202340e2c0bf5f1b47
June B <jrbergeron823@gmail.com>
2024-10-19 20:57:31 -0500

    Revert back to default signs

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,24 +1,25 @@
 return {
-	-- gitsigns config ...
 	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			signs = {
-				add          = { text = '┃' },
-				change       = { text = '┃' },
-				delete       = { text = '_' },
-				topdelete    = { text = '‾' },
-				changedelete = { text = '~' },
-				untracked    = { text = '┆' },
-			},
-			signs_staged = {
-				add          = { text = '┃' },
-				change       = { text = '┃' },
-				delete       = { text = '_' },
-				topdelete    = { text = '‾' },
-				changedelete = { text = '~' },
-				untracked    = { text = '┆' },
-			},
-		})
-	end
+	opts = {
+		signs = {
+		--	add          = { text = '+' },
+		--	change       = { text = '~' },
+			add          = { text = '┃' },
+			change       = { text = '┃' },
+			delete       = { text = '_' },
+			topdelete    = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked    = { text = '┆' },
+		},
+		signs_staged = {
+		--	add          = { text = '+' },
+		--	change       = { text = '~' },
+			add          = { text = '┃' },
+			change       = { text = '┃' },
+			delete       = { text = '_' },
+			topdelete    = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked    = { text = '┆' },
+		},
+	},
 }
