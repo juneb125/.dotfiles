1a1209d79c43ac7831727637bd8403b3f00aac55
June B <jrbergeron823@gmail.com>
2024-10-22 00:38:13 -0500

    Weird things going on with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,14 @@
 return {
 	"nvim-telescope/telescope.nvim", tag = '0.1.8',
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- "nvim-treesitter/nvim-treesitter"
+		-- "BurntSushi/ripgrep",
+	},
 	config = function()
 		-- local builtin = require("telescope.builtin")
-		-- vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Fuzzy find files" })
-		vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { desc = "Fuzzy find files" })
+		-- vim.keymap.set('n', '<C-p>', builtin.find_files({ hidden=true }), { desc = "Fuzzy find files" })
+		vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { silent = true, desc = "Fuzzy find files" })
 		-- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', {})
 	end
 }
