edfde3638fd1db2f6cd5bc02f50aa96a7d555db2
June B <jrbergeron823@gmail.com>
2024-10-22 00:17:19 -0500

    Redo telescope keymaps b/c they weren't working :(

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,9 +2,9 @@ return {
 	"nvim-telescope/telescope.nvim", tag = '0.1.8',
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<C-p>', builtin.find_files({ hidden = true }), { desc = "Fuzzy find files" })
-		-- vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		-- local builtin = require("telescope.builtin")
+		-- vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Fuzzy find files" })
+		vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { desc = "Fuzzy find files" })
+		-- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', {})
 	end
 }
