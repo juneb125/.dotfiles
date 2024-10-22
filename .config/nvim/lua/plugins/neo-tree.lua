96e7bd2b9c62287f7f155052e888c1e9dcb657fb
June B <jrbergeron823@gmail.com>
2024-10-22 01:13:56 -0500

    Add keymap to more easily change dir's w/ Neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,8 +8,10 @@ return {
       -- "3rd/image.nvim", -- optional image support in preview window
 		},
 		config = function()
-			vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>', {})
-			vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>', {})
-			-- vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
+			vim.keymap.set('n', '<C-n>', '<cmd>Neotree filesystem reveal left toggle<CR>', { silent = true, desc = "Toggle Neotree (left)" })
+			vim.keymap.set('n', '<leader>e', '<cmd>Neotree focus<CR>', { silent = true, desc = "Focus Neotree" })
+			vim.keymap.set('n', '<leader>ta', function() -- maybe change `ta` to something else
+				vim.fn.feedkeys(":Neotree dir=") -- feeds `:Neotree dir=` into the cmd line in CMD mode
+			end, { desc = "Interactively change directories with Neotree" })
 		end
 }
