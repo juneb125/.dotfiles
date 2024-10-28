2e0c804c48fa69b446b6bbf228abc09d8ddc95c6
June B <jrbergeron823@gmail.com>
2024-10-28 11:54:05 -0500

    Configure general LSP plugin settings

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -2,26 +2,24 @@ return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
-			require('mason').setup()
+			require("mason").setup({})
 		end
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require('mason-lspconfig').setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end
+		opts = {
+			ensure_installed = { "lua_ls" },
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
-			-- j
-			vim.keymap.set("n", "K", vim.lsp.buf.hover(), {})
-			-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			-- vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Info on hover" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action menu" })
 		end
 	},
 }
