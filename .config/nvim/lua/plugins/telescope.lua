cfa2c87f23c6b6ee17c43a72d63b8ddde520c4bb
June B <jrbergeron823@gmail.com>
2024-10-28 01:09:57 -0500

    Lazy loading, more keymaps, moved from config fn to  field Convert to lazy loading on keymap trigger, Add keymaps for showing Vim buffers and Vim registers via Telescope, Move all settings to  field, instead of the config function, Change default mode in Telescope file search to  instead of

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,23 @@
 return {
 	"nvim-telescope/telescope.nvim", tag = '0.1.8',
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { silent = true, desc = "Fuzzy find files" })
-		-- local builtin = require("telescope.builtin")
-		-- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', {})
-	end
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- "BurntSushi/ripgrep", -- necessary for live_grep and grep_string
+	},
+	opts = {
+		defaults = { initial_mode = 'normal' },
+	},
+	keys = {
+		{ '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', silent = true, desc = "Telescope fuzzy find files" },
+		{ '<leader>fb', '<cmd>Telescope buffers<CR>', silent = true, desc = 'Telescope buffers' },
+		{ '<leader>fr', '<cmd>Telescope registers<CR>', silent = true, desc = 'Telescope registers' },
+	},
+	-- config = function()
+		-- defaults = { initial_mode = 'normal' }
+		-- vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { silent = true, desc = "Telescope fuzzy find files" })
+		-- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep hidden=true<CR>', { silent = true, desc = 'Telescope live grep' })
+		-- vim.keymap.set('n', '<leader>fs', '<cmd>Telescope grep_string<CR>', { silent = true, desc = 'Telescope grep string under cursor' })
+		-- vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { silent = true, desc = 'Telescope buffers' })
+		-- vim.keymap.set('n', '<leader>fr', '<cmd>Telescope registers<CR>', { silent = true, desc = 'Telescope registers' })
+	-- end
 }
