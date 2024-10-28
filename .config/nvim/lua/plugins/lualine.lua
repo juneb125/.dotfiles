0339fb8f0fd420101269555d09f0782bf9d88fcc
June B <jrbergeron823@gmail.com>
2024-10-28 01:04:33 -0500

    Move everything in config fn to  field

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,13 +3,11 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons"
 	},
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = 'palenight'
-				-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-				-- possible other themes: modus-vivendi, ayu_mirage, nightfly, onedark?, etc.
-			}
-		})
-	end
+	opts = {
+		options = {
+			theme = 'palenight'
+			-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+			-- possible other themes: modus-vivendi, ayu_mirage, nightfly, onedark?, etc.
+		},
+	},
 }
