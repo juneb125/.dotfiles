795fe5a96a09d861faf31cd149fc36c4ebef0060
June B <jrbergeron823@gmail.com>
2024-10-28 01:06:26 -0500

    Prepare to move toward lazy loading w/ keymaps

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- optional image support in preview window
 		},
+		-- keys = {
+			-- { '<C-n>', '<cmd>Neotree filesystem reveal left toggle<CR>', silent = true, desc = "Toggle Neotree (left)" },
+			-- { '<leader>e', '<cmd>Neotree focus<CR>', silent = true, desc = "Focus Neotree" },
+			-- { '<leader>ta', function()
+				-- vim.fn.feedkeys(":Neotree dir=")
+			-- end, desc = "Interactively change directories with Neotree" },
+		-- }
 		config = function()
 			vim.keymap.set('n', '<C-n>', '<cmd>Neotree filesystem reveal left toggle<CR>', { silent = true, desc = "Toggle Neotree (left)" })
 			vim.keymap.set('n', '<leader>e', '<cmd>Neotree focus<CR>', { silent = true, desc = "Focus Neotree" })
