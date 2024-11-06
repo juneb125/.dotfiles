00ac95e6471a06133da410d6011ed5c4e9893464
June B <jrbergeron823@gmail.com>
2024-11-06 11:46:47 -0600

    Edit scope settings

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,11 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		scope = {
+			enabled = true,
+			show_start = false,
+			show_end = false,
+		},
+	},
 }
