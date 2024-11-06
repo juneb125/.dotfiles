d632653b956c9cef80544adf984bd6927a485c8c
June B <jrbergeron823@gmail.com>
2024-11-06 11:45:01 -0600

    Add new keymaps

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,9 +1,7 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end
+		opts = {},
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
@@ -15,10 +13,13 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+
 			lspconfig.lua_ls.setup({})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Info on hover" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
+			vim.keymap.set("n", "Y", vim.lsp.buf.hover, { desc = "Info on hover" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Jump to references" })
+			vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action menu" })
 		end
 	},
