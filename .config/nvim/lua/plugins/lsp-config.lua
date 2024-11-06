f4a9b93567049d3b1c544d94124e2585334daa48
June B <jrbergeron823@gmail.com>
2024-11-06 12:30:58 -0600

    Make format function async

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -19,7 +19,10 @@ return {
 			vim.keymap.set("n", "Y", vim.lsp.buf.hover, { desc = "Info on hover" })
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Jump to references" })
-			vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
+			vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+				vim.lsp.buf.format { async = true }
+			end, { desc = "Format code" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename variable" })
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action menu" })
 		end
 	},
