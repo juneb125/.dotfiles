e045d6e9b0fefb409f5bec0ce7b459630cdb270d
June B <jrbergeron823@gmail.com>
2024-11-06 11:46:06 -0600

    Edit fileformat section default settings

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,9 +5,19 @@ return {
 	},
 	opts = {
 		options = {
-			theme = 'palenight'
+			theme = 'palenight',
 			-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
 			-- possible other themes: modus-vivendi, ayu_mirage, nightfly, onedark?, etc.
+			sections = {
+				lualine_x = { 'encoding', {
+					'fileformat',
+					symbols = {
+						unix = 'unix', -- default: linux penguin icon
+						dos = 'dos',
+						mac = 'mac',
+					},
+				}, 'filetype' },
+			},
 		},
 	},
 }
