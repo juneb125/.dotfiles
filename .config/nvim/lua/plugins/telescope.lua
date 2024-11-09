33409e125197a9c5200e3c2ca633a2108aa16653
June B <jrbergeron823@gmail.com>
2024-11-08 21:24:58 -0600

    Remove comments of all former telescope-related keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,14 +13,6 @@ return {
 			{ '<leader>fb', '<cmd>Telescope buffers<CR>', silent = true, desc = 'Telescope buffers' },
 			{ '<leader>fr', '<cmd>Telescope registers<CR>', silent = true, desc = 'Telescope registers' },
 		},
-		-- config = function()
-			-- defaults = { initial_mode = 'normal' }
-			-- vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { silent = true, desc = "Telescope fuzzy find files" })
-			-- vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep hidden=true<CR>', { silent = true, desc = 'Telescope live grep' })
-			-- vim.keymap.set('n', '<leader>fs', '<cmd>Telescope grep_string<CR>', { silent = true, desc = 'Telescope grep string under cursor' })
-			-- vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { silent = true, desc = 'Telescope buffers' })
-			-- vim.keymap.set('n', '<leader>fr', '<cmd>Telescope registers<CR>', { silent = true, desc = 'Telescope registers' })
-		-- end
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
