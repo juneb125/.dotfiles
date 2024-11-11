fc8f61148f07cf1e77e1536572af13536f8a7d38
June B <jrbergeron823@gmail.com>
2024-11-10 20:40:14 -0600

    Change branch icon, fix un-responsiveness, format

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,15 +8,29 @@ return {
 			theme = 'palenight',
 			-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
 			-- possible other themes: modus-vivendi, ayu_mirage, nightfly, onedark?, etc.
-			sections = {
-				lualine_x = { 'encoding', {
+		},
+		sections = {
+			lualine_b = {
+				{
+					'branch',
+					icons_enabled = false,
+					fmt = function(str)
+						return ' ' .. str
+					end,
+				},
+				'diff', 'diagnostics'
+			},
+
+			lualine_x = { 'encoding',
+				{
 					'fileformat',
 					symbols = {
 						unix = 'unix', -- default: linux penguin icon
 						dos = 'dos',
 						mac = 'mac',
 					},
-				}, 'filetype' },
+				},
+				'filetype'
 			},
 		},
 	},
