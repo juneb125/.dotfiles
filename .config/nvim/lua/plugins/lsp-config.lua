33a91e5c076a69c67804a751d1b281a0d20a25c9
June B <jrbergeron823@gmail.com>
2024-11-11 17:50:48 -0600

    Add Nix and Rust LSP's

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls" },
+			ensure_installed = { "lua_ls", "nil_ls", "rust_analyzer" },
 		},
 	},
 	{
@@ -14,15 +14,14 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
+			lspconfig.lua_ls.setup({}) -- lua
+			lspconfig.nil_ls.setup({}) -- nix
+			lspconfig.rust_analyzer.setup({})-- rust
 
-			vim.keymap.set("n", "Y", vim.lsp.buf.hover, { desc = "Info on hover" })
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Info on hover" })
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Jump to references" })
-			vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-				vim.lsp.buf.format { async = true }
-			end, { desc = "Format code" })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename variable" })
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action menu" })
 		end
 	},
