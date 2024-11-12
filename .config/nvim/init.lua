3b73d1a993b0b4920e02a364aeef7925ca42409d
June B <jrbergeron823@gmail.com>
2024-11-12 11:20:20 -0600

    Format and change 'change_detection.notify' to true

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,24 +1,24 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"--branch=stable",
 		lazyrepo,
-		lazypath
+		lazypath,
 	})
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -28,12 +28,12 @@ require("config.keymaps") -- keymaps
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {{ import = "plugins" }},
+	spec = { { import = "plugins" } },
 	-- configure any other settings here
 	install = { colorscheme = { "catppuccin" } }, -- colorscheme that will be used when installing plugins.
-  checker = {
+	checker = {
 		enabled = true, -- automatically check for plugin updates,
 		notify = true, -- and then notify us
 	},
-	change_detection = { notify = false },
+	change_detection = { notify = true },
 })
