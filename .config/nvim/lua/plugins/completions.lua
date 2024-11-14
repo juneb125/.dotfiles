1949f1d174e159efc241bf6a044ab9f402aa436a
June B <jrbergeron823@gmail.com>
2024-11-13 22:09:57 -0600

    Try to edit keymaps

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -27,9 +27,11 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					-- make these keymaps work !! <--
+					["<C-j>"] = cmp.mapping.scroll_docs(-4),
+					["<C-k>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
+					-->
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
