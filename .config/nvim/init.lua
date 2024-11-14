4ba1dba3855cf5f275fe356365cf8fbea37bfc54
June B <jrbergeron823@gmail.com>
2024-11-13 22:57:28 -0600

    Simplify setup, disable Netrw Insert 'lazyrepo''s value into the setup function, rather than use the variable Disable Netrw, Vim's default file browser/tree thing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,12 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
-		lazyrepo,
 		lazypath,
 	})
 	if vim.v.shell_error ~= 0 then
@@ -22,6 +21,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- disable Vim's built-in file explorer
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Vim settings
 require("config.vim-options") -- base customizations
 require("config.keymaps") -- keymaps
@@ -30,7 +33,7 @@ require("config.keymaps") -- keymaps
 require("lazy").setup({
 	spec = { { import = "plugins" } },
 	-- configure any other settings here
-	install = { colorscheme = { "catppuccin" } }, -- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "catppuccin" } }, -- colorscheme that will be used when installing plugins
 	checker = {
 		enabled = true, -- automatically check for plugin updates,
 		notify = true, -- and then notify us
