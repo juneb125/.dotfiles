354a081f0d3b22d77b72d3bbf37531e96d72b8c8
June B <jrbergeron823@gmail.com>
2024-11-15 23:28:28 -0600

    Finally make keymaps work

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -26,15 +26,27 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
-				mapping = cmp.mapping.preset.insert({
+				mapping = {
 					-- make these keymaps work !! <--
-					["<C-j>"] = cmp.mapping.scroll_docs(-4),
-					["<C-k>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-j>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end,
+					["<C-k>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end,
 					-->
+					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
+				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
@@ -42,6 +54,9 @@ return {
 					{ name = "buffer" },
 				}),
 			})
+			-- vim.keymap.set("i", "<C-Tab>", cmp.mapping.scroll_docs(-1), {})
+			-- vim.keymap.set("i", "<C-`>", cmp.mapping.scroll_docs(1), {})
+			-- vim.keymap.set("i", "<C-Space>", cmp.mapping.complete(), {})
 		end,
 	},
 }
