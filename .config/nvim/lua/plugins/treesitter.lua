d072fde321180c643cc1ebb1b034bbc69b60a6bf
June B <jrbergeron823@gmail.com>
2024-11-15 23:29:08 -0600

    Add 'rust' to list of ensure installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "lua" },
+			ensure_installed = { "lua", "rust" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
