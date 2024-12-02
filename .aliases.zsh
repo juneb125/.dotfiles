4cdead62abcea9ccb43869a16c1a67bf4f640e29
June B <jrbergeron823@gmail.com>
2024-12-01 23:28:14 -0600

    Move all aliases from '.zshrc' into here to reduce clutter :)

---
+++ .aliases.zsh
@@ -0,0 +1,26 @@
+# -- Aliases --
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias proj='cd ~/Desktop/Coding/ && ls -aG'
+
+# 'ls'
+alias la='ls -aG'
+alias ll='ls -lG'
+alias lal='ls -alG'
+
+# Git
+alias ga='git add'
+alias gb='git branch'
+alias gs='git status'
+alias gl='git log --graph'
+
+# Vim / Nvim
+alias nv='nvim'
+alias vi='nvim'
+alias reg_vim='vim'
+
+# Misc.
+alias cl='clear'
+alias rebuild_nix_flake='darwin-rebuild switch --flake ~/.dotfiles/.config/nix-darwin#Junes-MacBook-Air'
