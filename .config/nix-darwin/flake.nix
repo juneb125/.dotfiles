e39474350b4078fc647e2b16154c59be1f812132
June B <jrbergeron823@gmail.com>
2024-12-13 20:16:57 -0600

    Move everything in configuration (in let-in statement) to './config.nix'

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -2,21 +2,26 @@
   description = "June's Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, darwin, nixpkgs }:
   let
-		configuration = import ./config.nix;
+		configuration = { pkgs, ... }: {
+			imports = [
+				(import ./config.nix { inherit self pkgs; })
+			];
+		};
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild switch --flake path/to/nix-darwin#Your-Flake-Name
-    darwinConfigurations."Junes-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations."Junes-MacBook-Air" = darwin.lib.darwinSystem {
+			# system = "aarch64-darwin";
+			modules = [ configuration ];
     };
   };
 }
