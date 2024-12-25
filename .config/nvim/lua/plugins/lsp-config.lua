f01f4dc7a78e8b7ec0c0b32800b19c1170b76320
June B <jrbergeron823@gmail.com>
2024-12-25 03:14:40 -0600

    Clean up format keymapping

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -25,10 +25,7 @@ return {
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Jumps to definition of the symbol under the cursor" })
 			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Lists all the implementations for the symbol under the cursor" })
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Lists all the references to the symbol under the cursor" })
-			vim.keymap.set("n", "<leader>gf", function()
-				vim.lsp.buf.format()
-				print("Formatted!")
-			end, { desc = "Formats current buffer using the attached LSP client(s)" })
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Formats current buffer using the attached LSP client(s)" })
 
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Selects a code action available at the current cursor position" })
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Renames all references to the symbol under the cursor" })
