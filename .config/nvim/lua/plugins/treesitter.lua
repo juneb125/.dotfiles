ca5f840d0e59cd38c51950c692c03d8eead7b769
June B <jrbergeron823@gmail.com>
2025-01-07 22:58:49 -0600

    Add Markdown to 'ensure_installed' table

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "lua", "rust" },
+			ensure_installed = { "lua", "rust", "markdown" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
