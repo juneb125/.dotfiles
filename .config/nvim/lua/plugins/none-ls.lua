3909e862b5e62827ce4a319ff7da5e4dc3ebc70a
June B <jrbergeron823@gmail.com>
2025-02-10 15:23:46 -0600

    Assign 'none_ls.builtins' to a variable to reduce repitition & verbosity

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -2,12 +2,13 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+		local builtins = null_ls.builtins
 
 		null_ls.setup({
 			sources = {
 				-- formatting
-				null_ls.builtins.formatting.stylua, -- lua
-				null_ls.builtins.formatting.nixfmt, -- nix
+				builtins.formatting.stylua, -- lua
+				builtins.formatting.nixfmt, -- nix
 				-- rust: included with rust_analyzer (see lsp-config.lua)
 			},
 		})
