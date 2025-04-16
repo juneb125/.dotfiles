2b44d9c62bb53fed96d9bc9cb90982a183724ff9
June B <jrbergeron823@gmail.com>
2025-04-15 19:45:14 -0500

    Format w/ alejandra :D

--- .config/nix-darwin/config.nix
+++ .config/nix-darwin/config.nix
@@ -3,71 +3,71 @@
 
 	# imports = [];
 
-	nixpkgs = {
-		config.allowUnfree = true;       # allow unfree (i.e., not open-source) pkgs
-		hostPlatform = "aarch64-darwin"; # platform the config will be used on
-	};
+  nixpkgs = {
+    config.allowUnfree = true; # allow unfree (i.e., not open-source) pkgs
+    hostPlatform = "aarch64-darwin"; # platform the config will be used on
+  };
 
-	environment = {
-		shells = [ pkgs.zsh ];
+  environment = {
+    shells = [ pkgs.zsh ];
 
-		# search for packages in https://search.nixos.org/packages
-		systemPackages = (with pkgs; [
-				# bat
-				erlang_27   # required for gleam
-				fastfetch
-				git
-				neovim
-				rebar3
-				starship # prompt customization
-		]) ++ (with pkgs-unstable; [
-			deno
-			gleam
-		]);
+    # search for packages in https://search.nixos.org/packages
+    systemPackages = (with pkgs; [
+        # bat
+        erlang_27 # required for gleam
+        fastfetch
+        git
+        neovim
+        rebar3
+        starship # prompt customization
+      ]) ++ (with pkgs-unstable; [
+        deno
+        gleam
+      ]);
 
-		variables = {
-			SHELL = lib.getExe pkgs.zsh;
-		};
-	};
+    variables = {
+      SHELL = lib.getExe pkgs.zsh;
+    };
+  };
 
-	fonts.packages = with pkgs; [
-		jetbrains-mono
-	];
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+  ];
 
-	# necessary for using flakes
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # necessary for using flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-	# set git commit hash for darwin-version
-	system.configurationRevision = inputs.rev or inputs.dirtyRev or null;
+  # set git commit hash for darwin-version
+  system.configurationRevision = inputs.rev or inputs.dirtyRev or null;
 
-	# used for backwards compatibility, read the changelog before changing
-	# $ darwin-rebuild changelog
-	system.stateVersion = 5;
+  # used for backwards compatibility, read the changelog before changing
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
 
-	# auto upgrade nix pkg and the daemon service
-	services.nix-daemon.enable = true;
-	# nix.package = pkgs.nix;
-	
-	programs.zsh = {
-		enable = true;
-	};
+  # auto upgrade nix pkg and the daemon service
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-	nixpkgs.config.git = {
-		enable = true;
-		userName = "juneb125";
-		userEmail = "jrbergeron823@gmail.com";
-		ignores = [
-			"*.DS_Store"
-			"*.swp"
-			"*.tmp*"
-		];
-		config = {
-			init = { defaultBranch = "main"; };
-		};
-	};
+  programs.zsh = {
+    enable = true;
+  };
 
-	nixpkgs.config.neovim = {
-		enable = true;
-		defaultEditor = true;
-	};
+  nixpkgs.config.git = {
+    enable = true;
+    userName = "juneb125";
+    userEmail = "jrbergeron823@gmail.com";
+    ignores = [
+      "*.DS_Store"
+      "*.swp"
+      "*.tmp*"
+    ];
+    config = {
+      init = { defaultBranch = "main"; };
+    };
+  };
+
+  nixpkgs.config.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
 }
--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -5,27 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin = {
-			url = "github:LnL7/nix-darwin";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, darwin, nixpkgs, nixpkgs-unstable, ... }:
-  let
-		inherit (self) outputs;
-		# localHostName = "Junes-MacBook-Air";
-		system = "aarch64-darwin";
-  in
-  {
+	let
+    inherit (self) outputs;
+    # localHostName = "Junes-MacBook-Air";
+    system = "aarch64-darwin";
+  in {
     # (re-)build darwin flake using:
     # $ darwin-rebuild switch --flake path/to/nix-darwin#Your-Flake-Name
     darwinConfigurations."Junes-MacBook-Air" = darwin.lib.darwinSystem {
-			inherit system;
-			modules = [ ./config.nix ];
-			specialArgs = {
-				inherit inputs outputs;
-				pkgs-unstable = import nixpkgs-unstable { system = system; };
-			};
+      inherit system;
+      modules = [ ./config.nix ];
+      specialArgs = {
+        inherit inputs outputs;
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+      };
     };
   };
 }
