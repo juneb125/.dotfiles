43d62aaafca03262fc188612589363bd870592d1
June B <jrbergeron823@gmail.com>
2025-05-27 11:24:31 -0500

    Setup automatic garbage collection

--- .config/nix/modules/nix.nix
+++ .config/nix/modules/nix.nix
@@ -3,7 +3,16 @@
   nixpkgs.config.allowUnfree = true;
 
   # necessary for using flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  nix.gc = {
+    automatic = true;
+		# bimonthly
+    interval = [
+      {Day = 1;}
+      {Day = 15;}
+    ];
+  };
 
   # auto upgrade nix pkg and the daemon service
   services.nix-daemon.enable = true;
