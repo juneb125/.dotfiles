4e068073b374eb4c4e1e5de7c14e4155a2f9eeb5
June B <jrbergeron823@gmail.com>
2025-07-22 21:56:59 -0500

    Add preview() fn

--- .config/nix/modules/shell/macos.sh
+++ .config/nix/modules/shell/macos.sh
@@ -26,3 +26,18 @@ finder() {
   return $?
 }
 
+preview() {
+  case "$1" in;
+    "") open $HOME -a Preview.app ;;
+    *) if [[ -f $1 ]]; then
+      open $1 -a Preview.app
+    elif [[ -d $1 ]]; then
+      echo "preview: can't open $1 because it's a directory"
+      return 1
+    else
+      echo "preview: either $1 doesn't exist, "
+      return 1
+    fi ;;
+  esac
+  return $?
+}
