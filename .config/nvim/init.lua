8860dfb4b50ad7c3ec52aafbf33af723fae6cf96
June B <jrbergeron823@gmail.com>
2025-08-07 17:45:23 -0500

    Format, add some LSP-related keymaps, and add & configure Mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,12 +5,22 @@ require("settings")
 vim.g.mapleader = " "
 
 local map = vim.keymap.set
-map("n", "<leader>o", ":update<CR>:source<CR>")
-map("n", "<leader>nh", "<cmd>nohlsearch<CR>")
+map("n", "<leader>o", "<cmd>update<CR> <cmd>source<CR>")
+map("n", "<leader>nh", "<cmd>nohlsearch<CR>", {})
+
+local buf = vim.lsp.buf
+map("n", "K", buf.hover)
+map("n", "<leader>gf", buf.format)
+map("n", "<leader>ca", buf.code_action)
 
 -- Plugins --
 vim.pack.add({
-		{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 })
+
+vim.lsp.enable({ "lua_ls" })
+
+require("mason").setup()
 
 vim.cmd("colorscheme catppuccin")
