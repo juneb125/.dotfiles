68f680e394cf9153848db41a26269541c701f3d6
June B <jrbergeron823@gmail.com>
2025-08-13 18:51:04 -0500

    Move crediting info to `CREDITS.md`

---
+++ .config/nvim/CREDITS.md
@@ -0,0 +1,5 @@
+# Credits
+
+1. [`typecraft-dev/dotfiles`](https://github.com/typecraft-dev/dotfiles/blob/master/nvim/.config/nvim/lua/vim-options.lua) (path: `/nvim/.config/nvim/lua/vim-options.lua`)
+2. [`josean-dev/dev-environment-files`](https://github.com/josean-dev/dev-environment-files/blob/main/.config/nvim/lua/josean/core/keymaps.lua) (path: `/.config/nvim/lua/josean/core/keymaps.lua`)
+3. [`nmasur/dotfiles`](https://github.com/nmasur/dotfiles/blob/master/pkgs/applications/editors/neovim/nmasur/neovim/lua/keybinds.lua) (path: `pkgs/applications/editors/neovim/nmasur/neovim/lua/keybinds.lua`)
--- .config/nvim/keymaps.lua
+++ .config/nvim/keymaps.lua
@@ -7,24 +7,20 @@ local map = vim.keymap.set
 
 map("n", "<leader>wt", "<cmd>set wrap!<CR>")
 
--- navigate Vim panes better, from
--- github:typecraft-dev/dotfiles/nvim/.config/lua/vim-options.lua
+-- navigate Vim panes better, see Credits #1
 map("n", "<C-k>", "<cmd>wincmd k<CR>")
 map("n", "<C-j>", "<cmd>wincmd j<CR>")
 map("n", "<C-h>", "<cmd>wincmd h<CR>")
 map("n", "<C-l>", "<cmd>wincmd l<CR>")
 
--- from github:josean-dev/dev-environment-files
--- ^ path:/.config/nvim/lua/josean/core/keymaps.lua
+-- clear highlighting, see Credits #2
 map("n", "<leader>nh", "<cmd>nohlsearch<CR>")
 
--- keep selection when changing indentation
--- from github:nmasur/dotfiles/pkgs/applications/editors/neovim/nmasur/neovim/lua/keybinds.lua
+-- keep selection when changing indentation, see Credits #3
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- shuffle lines around
--- from the person who wrote the 2 keymaps above
+-- shuffle lines around, see Credits #3
 map("n", "<A-j>", "<cmd>m .+1<CR>==")
 map("n", "<A-k>", "<cmd>m .-2<CR>==")
 
