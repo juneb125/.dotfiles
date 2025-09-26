ca03510708fbd87b7f639afa45eb463ee2611008
June B <jrbergeron823@gmail.com>
2025-09-26 18:35:35 -0500

    Add alias for `cat` -> `bat --plain`

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -26,4 +26,8 @@ alias vim='nvim'
 alias c='clear'
 alias e='exit 0'
 alias scerr='scutil --error $?'
+
+if [[ $(which bat) ]]; then
+	alias cat='bat --plain'
+fi
 # alias clang-c='clang -std=c99 -Wall -Werror '
