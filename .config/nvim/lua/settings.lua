a9eb320dfef2b6634e46141cef14c526cabeab06
June B <jrbergeron823@gmail.com>
2025-10-29 01:42:20 -0500

    nvim: Bring back my preferred window splitting behavior (from before the overhaul)

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -14,12 +14,16 @@ opt.shiftwidth = 2 -- number of spaces to use for each step of (auto)indent
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
-          
+
 -- misc. settings
 opt.mouse = "a"          -- enable mouse clicks
 opt.wrap = false         -- turn off line wrap by default
 opt.termguicolors = true -- use GUI colors for the terminal
 opt.signcolumn = "yes"   -- show sign column so that text doesn't shift
 opt.swapfile = false     -- turn off swapfile
+
+-- default split window behavior
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
