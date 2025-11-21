d83de86e280b7009400f961aa7aea3c20c7036b8
June B <jrbergeron823@gmail.com>
2025-11-21 02:45:11 -0600

    zsh: Super small improvements to `get-plugins.zsh`

--- sources/zsh/get-plugins.zsh
+++ sources/zsh/get-plugins.zsh
@@ -1,18 +1,16 @@
-if [[ ! -n ${ZPLUGINDIR} ]]; then
+if [[ -z ${ZPLUGINDIR} ]]; then
   echo "\x1b[0;31mZPLUGINDIR must be set in order to download / update the plugins\x1b[m"
   return 1
 fi
 
 get-syntax-highlighting() {
   local dest="${ZPLUGINDIR}/zsh-syntax-highlighting"
-  [[ -d ${dest} ]] || mkdir -p ${dest}
 
   # return early if plugin is already installed
   [[ -d ${dest}/.git ]] && return 0
 
   echo "\x1b[0;33m${0}: downloading zsh-syntax-highlighting...\x1b[m"
   local repo="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-
   git clone ${repo} ${dest} --depth 1
   local res=$?
 
