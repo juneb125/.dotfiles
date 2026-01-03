b3e6e55667edad6e9f1282ab4e916ec080a17f89
June B <jrbergeron823@gmail.com>
2026-01-02 23:56:56 -0600

    fix(zsh): Remove `finder` function from `macos.sh` because open(1) just does that

--- sources/zsh/macos.sh
+++ sources/zsh/macos.sh
@@ -9,15 +9,6 @@ xcode() {
   fi
 }
 
-finder() {
-  if [[ -z "${1}" || -e ${1} ]]; then
-    open ${1} -a Finder.app
-  else
-    echo "${0}: ${1} doesn't exist"
-    return 1
-  fi
-}
-
 sm() {
   local domain="com.apple.WindowManager GloballyEnabled"
 
