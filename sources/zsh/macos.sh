0fe52fcd9ac8ee134f114d1ca0e4b45a7a2eb371
June B <jrbergeron823@gmail.com>
2026-01-02 23:45:45 -0600

    feat(zsh): Add `sm` function in `macos.sh` that gets/sets Stage Manager state

--- sources/zsh/macos.sh
+++ sources/zsh/macos.sh
@@ -17,3 +17,33 @@ finder() {
     return 1
   fi
 }
+
+sm() {
+  local domain="com.apple.WindowManager GloballyEnabled"
+
+  case "$1" in;
+    "--help")
+      cat <<EOF
+Read and write Stage Manager settings
+
+to read:   ${0}
+to write:  ${0} [true|false]
+EOF
+      return 0
+      ;;
+    "")
+      if [[ "$(eval "defaults read ${domain}")" == "0" ]]; then
+        echo "stage manager is off (0)"
+      else
+        echo "stage manager is on (1)"
+      fi
+      ;;
+    "true"|"false")
+      eval "defaults write ${domain} -bool ${1}"
+      ;;
+    *)
+      echo "${0}: invalid input: ${1}"
+      return 1
+      ;;
+  esac
+}
