f2ebad86bb75c8fe8974af79cec1578034a1a11d
June B <jrbergeron823@gmail.com>
2026-02-01 15:03:02 -0600

    chore(nix): update nix version of starship's config file (see previous commit)

--- modules/starship.nix
+++ modules/starship.nix
@@ -3,55 +3,46 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
+      # Inserts a blank line between shell prompts
       add_newline = true;
-      format = ''
-        $directory $git_branch$git_status
-        $character
-      ''; # PS1
+      format = "$directory $git_branch$git_status\n$character"; # PS1
       right_format = "$cmd_duration"; # RPROMPT
       continuation_prompt = "| "; # PS2
 
-      palette = "ansi";
+      # Custom palette for storing color variables
+      palette = "vars";
+      # just for the foreground of git stuff
+      palettes = {
+        vars.git_gray = "$6c6c6c";
+      };
+
+      # Disable the package module, hiding it from the prompt completely
+      package.disabled = true;
+
       character = {
-        success_symbol = "\n[❯](fg:magenta)";
-        error_symbol = "\n[❯](fg:red)";
+        # 'purple' is more of a pale pink with Catppuccin Macchiato
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
       };
 
       directory = {
         truncation_length = 3;
         truncation_symbol = "";
-        format = "[$path](fg:blue)";
+        format = "[$path](blue)";
       };
 
-      git_branch.format = "[$branch](fg:git_gray)";
+      git_branch.format = "[$branch](git_gray)";
       git_status = {
         ahead = "↑";
         behind = "↓";
         modified = "*";
-        format = "[$modified $ahead_behind](fg:git_gray)";
+        format = "[$modified $ahead_behind](git_gray)";
       };
 
       cmd_duration = {
         min_time = 2000;
         show_milliseconds = false;
-        format = "[$duration](fg:yellow)";
-      };
-
-      package.disabled = true;
-
-      palettes.ansi = {
-        # ANSI (reassigned by Catppuccin Macchiato)
-        # black = "0"
-        red = "1";
-        # green = "2"
-        yellow = "3";
-        blue = "4";
-        magenta = "5";
-        # cyan = "6"
-        # white = "7"
-        # gray = "8"
-        # just for the foreground of git stuff
-        git_gray = "#6c6c6c";
+        format = "[$duration](yellow)";
       };
     };
   };
