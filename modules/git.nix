87b8268649e4d5b2e584b5c825ea9ece2eabff1c
June B <jrbergeron823@gmail.com>
2026-02-01 20:08:06 -0600

    feat(nix): update `modules/git.nix` so that it matches `sources/git/config`

--- modules/git.nix
+++ modules/git.nix
@@ -1,31 +1,42 @@
 { ... }: {
-  nixpkgs.config.git = let
-    name = "juneb125";
-    token = builtins.getEnv "JUNES_GITHUB_TOKEN";
-  in {
+  nixpkgs.config.git = {
     enable = true;
-    userName = name;
-    userEmail = "jrbergeron823@gmail.com";
+    user = {
+      name = "June B";
+      email = "jrbergeron823" + "@" + "gmail" + "." + "com";
+      username = "juneb125";
+    };
     aliases = {
       br = "branch";
+      cam = "commit -am";
       cl = "clone";
       cm = "commit -m";
       co = "checkout";
       i = "init";
       log = "log --graph";
     };
+    # instead of core.excludesFile
     ignores = [
-      "*.DS_Store"
+      "**/.DS_Store"
       "*.swp"
-      "*.tmp*"
+      "*.tmp"
     ];
-    delta = {
-      enable = true;
-    };
     config = {
       init.defaultBranch = "main";
       core.editor = "nvim";
-      github = { inherit name token; };
+      # commit.template = "$XDG_CONFIG_HOME/git/template";
+      push = {
+        default = "current";
+        followTags = true;
+      };
+      pull.default = "current";
+      github.name = "juneb125";
+      credential.helper = "store";
+      url = {
+        "https://github.com/" = {
+          insteadOf = ["github:"];
+        };
+      };
     };
   };
 }
