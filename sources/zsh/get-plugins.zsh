79800cc9b8640cac3fb3d7210d07f62e46d24384
June B <jrbergeron823@gmail.com>
2026-02-26 22:58:22 -0600

    feat(zsh): add 'update' subcmd to (+ some restructuring of) `get-syntax-highlighting` fn

--- sources/zsh/get-plugins.zsh
+++ sources/zsh/get-plugins.zsh
@@ -9,20 +9,16 @@ fi
 
 get-syntax-highlighting() {
   local dest="${ZPLUGINDIR}/zsh-syntax-highlighting"
-
-  # return early if plugin is already installed
-  [[ -d "${dest}"/.git ]] && return 0
-
-  echo "\x1b[0;33m${0}: downloading zsh-syntax-highlighting...\x1b[m"
   local repo="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-  git clone "${repo}" "${dest}" --depth 1
-  local res=$?
 
-  if [[ ${res} ]]; then
-    echo "\x1b[0;32m${0}: download successful\x1b[m"
+  if [[ -d "${dest}"/.git ]]; then
+    # return early if already installed & no update arg
+    [[ "${1}" != up* ]] && return 0
+
+    echo "\x1b[0;33m${0}: updating zsh-syntax-highlighting...\x1b[m"
+    git -C "${dest}" pull "${repo}" --depth=1 -f
   else
-    echo "\x1b[0;31m${0}: download failed with exit code ${res}\x1b[m" >&2
+    echo "\x1b[0;33m${0}: downloading zsh-syntax-highlighting...\x1b[m"
+    git clone "${repo}" "${dest}" --depth=1
   fi
-
-  return ${res}
 }
