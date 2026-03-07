813542debd92140b503f928713d042aff6e0f732
June B <jrbergeron823@gmail.com>
2026-03-07 01:54:46 -0600

    perf(nvim): only calculate all of the plugin names once (+ store them in a global var) instead of having PackUpdate completion calculate it every time

--- sources/nvim/lua/commands.lua
+++ sources/nvim/lua/commands.lua
@@ -1,5 +1,11 @@
 -- User commands & Lua functions --
 
+--- The names of all the plugins registered to vim.pack
+---@type string[]
+vim.g.all_plugins = vim.iter(ipairs(vim.pack.get())):map(
+	function(_, v) return v.spec.name end
+):totable()
+
 vim.api.nvim_create_user_command("PackUpdateAll", vim.pack.update, { desc = "Update all vim.pack plugins" })
 
 vim.api.nvim_create_user_command("PackUpdate", function(opts)
@@ -8,9 +14,7 @@ end, {
 	nargs = "+",
 	desc = "Update specific vim.pack plugins",
 	complete = function(ArgLead, _, _)
-		local choices = vim.iter(ipairs(vim.pack.get())):map(
-				function(_, v) return v.spec.name end
-			):totable()
+		local choices = vim.g.all_plugins
 		if ArgLead == "" then
 			return choices
 		end
