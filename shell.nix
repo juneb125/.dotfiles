e8aa7e04cf2920ca8d31f442c4940f06a33854ba
June B <jrbergeron823@gmail.com>
2026-04-17 03:40:55 -0500

    feat(nix): add shell hook to describe what pkgs are available in `shell.nix`

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
+{ pkgs ? import <nixpkgs> {}, ... }:
+pkgs.mkShell rec {
   packages = with pkgs; [
     alejandra
     deadnix
@@ -9,4 +9,14 @@ pkgs.mkShell {
     shellcheck
     stylua
   ];
+
+  shellHook = ''
+    echo "the following packages are available:"
+    i=""
+    for i in ${builtins.toString packages}; do
+      echo -e "- ''${i#*-}"
+    done
+    unset i
+    echo ""
+  '';
 }
