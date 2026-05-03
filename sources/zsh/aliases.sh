2e5b249c1e25c09dd5eb8b155dede2bf65dd34b7
June B <jrbergeron823@gmail.com>
2026-05-03 10:06:32 -0500

    feat(zsh): add more aliases

--- sources/zsh/aliases.sh
+++ sources/zsh/aliases.sh
@@ -13,6 +13,7 @@ alias lal='ls -AlG'
 
 # Git
 alias ga='git add'
+alias gap='git add -p'
 alias gb='git branch'
 alias gco='git checkout'
 alias gd='git diff'
@@ -28,7 +29,12 @@ alias c='clear'
 alias e='exit 0'
 alias restart='reset'
 alias notes='cd ~/Documents/"Obsidian Vault"'
+alias bcurl='curl -fsSL' # [b]etter curl
 
 if which tree >/dev/null; then
   alias tree="command tree --gitignore -F"
+fi
+
+if which fastfetch >/dev/null; then
+  alias ff='fastfetch'
 fi
