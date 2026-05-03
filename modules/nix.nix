85917ae25a000412ca4b439b5a5858cbc2b6b297
June B <jrbergeron823@gmail.com>
2026-05-03 10:27:13 -0500

    feat(nix): set `nix.settings.warn-dirty` to false and enable nix man pages

--- modules/nix.nix
+++ modules/nix.nix
@@ -4,8 +4,17 @@
   # allow unfree (i.e., not open-source) pkgs
   nixpkgs.config.allowUnfree = true;
 
-  # necessary for using flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # ref: https://nix.dev/manual/nix/2.28/command-ref/conf-file.html
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"]; # necessary for using flakes
+    warn-dirty = false;
+  };
 
   # nix.package = pkgs.nix;
+  documentation = {
+    enable = true;
+    doc.enable = true;
+    info.enable = false;
+    man.enable = true;
+  };
 }
